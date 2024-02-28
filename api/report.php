83ef425ecf3f8d13784906d3ed2fd320efb6488f
mosu <peerakran03g@gmail.com>
2024-02-28 23:40:51 +0700

    api

--- api/report.php
+++ api/report.php
@@ -1,22 +1,8 @@
 <?php
-// api/report.php
+include('../config/connectdb.php');
 
-// Include necessary files and perform any required operations to fetch data
-// For example, fetching data from your database
-
-// Sample data for demonstration
-$xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
-$yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
-
-// Prepare data to be sent as JSON
-$data = [
-  'xValues' => $xValues,
-  'yValues' => $yValues
-];
-
-// Set response headers
 header('Content-Type: application/json');
 
-// Output JSON
+
 echo json_encode($data);
 ?>
