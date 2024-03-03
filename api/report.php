050157097a151fd421ec71876535748e0e768dea
mosu <peerakran03g@gmail.com>
2024-03-04 04:47:41 +0700

    mock up data

--- api/report.php
+++ api/report.php
@@ -1,5 +1,13 @@
 <?php
 include('../config/connectdb.php');
+$xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+$yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+
+// Prepare data to be sent as JSON
+$data = [
+  'xValues' => $xValues,
+  'yValues' => $yValues
+];
 
 header('Content-Type: application/json');
 
